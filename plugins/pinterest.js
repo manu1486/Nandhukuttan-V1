5fef0b92f78e699cafd64efa6e0e93ca39a56055
Benison-Achu <96046934+manu1486@users.noreply.github.com>
2022-04-25 14:46:19 +0530

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', '© ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
+  conn.sendFile(m.chat, pint, '', '© 𝙉𝘼𝙉𝘿𝙃𝙐𝙆𝙐𝙏𝙏𝘼𝙉 𝙑1', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
 }
 handler.help = ['pinterest <search>']
 handler.tags = ['internet']
